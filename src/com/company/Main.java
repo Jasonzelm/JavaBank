147e725d87cca545c335342edc375c64c518dfea
Jason Zelmanovich <Jasonzelm7@gmail.com>
2018-03-06 22:17:39 -0500

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //Menu bankMenu = new Menu();
-        
+        Menu bankMenu = new Menu();
+
     }
 }
