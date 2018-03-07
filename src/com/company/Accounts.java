5083c5505fb107343da70341e556afd8f22b3944
Jason Zelmanovich <Jasonzelm7@gmail.com>
2018-03-06 22:33:19 -0500

    commit

--- src/com/company/Accounts.java
+++ src/com/company/Accounts.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Accounts {
 
-    accntNumArry array = new accntNumArry();
+    accntNumArry array;
     private Scanner in = new Scanner(System.in);
     int accountNum;
     int pinNum;
@@ -14,26 +14,23 @@ public class Accounts {
     int intScanner;
     String stringScanner;
     int arrayNum;
-    int n;
 
     public Accounts(){
-        accntNumArry account = new accntNumArry();
 
         // ask for the account number with the scanner
         System.out.println("Enter your account number here: ");
         this.intScanner = in.nextInt();
-       //figure out how to make it look for the account number through an array
-        //or(n : array.accntArray()) {
 
+       //look for the account number through an array in accntNumarry class
+        for(int n : array.accntNums) {
             if (this.intScanner == n) {
                 System.out.println("We found your account");
-            } else {
+            } else if(this.intScanner != n) {
                 System.out.println("Your account doesnt seem to exist");
             }
-        //}
-        //ask for the account name(ex: Account1, Account2, etc) then set it to an account number here
-        System.out.println("Enter the account name (ex: Account1, Account2, etc): ");
-        this.stringScanner = in.nextLine();
+        }
+
+
     }
 
 
--- src/com/company/accntNumArry.java
+++ src/com/company/accntNumArry.java
@@ -3,21 +3,22 @@ package com.company;
 public class accntNumArry {
     Accounts account = new Accounts();
     int entrdAccntNum;
-    int Account1;
-    int Account2;
-    int Account3;
-    int Account4;
-    int Account5;
-    int Account6;
-    int Account7;
-    int Account8;
-    int Account9;
-    int Account10;
-    int Account11;
+    int[] accntNums;
+    int Account1 = 26028422;
+    int Account2 = 0;
+    int Account3 = 0;
+    int Account4 = 0;
+    int Account5 = 0;
+    int Account6 = 0;
+    int Account7 = 0;
+    int Account8 = 0;
+    int Account9 = 0;
+    int Account10 = 0;
+    int Account11 = 0;
 
-    public  int accntArray(){
-        int[] accntNums = new int[]{Account1, Account2, Account3, Account4, Account5,Account6, Account7, Account8, Account9, Account10, Account11};
-        return account.n;
+    public void accntArray(){
+        accntNums = new int[]{Account1, Account2, Account3, Account4, Account5,Account6, Account7, Account8, Account9, Account10, Account11};
+
     }
 
     public void setAccount(int num) {
