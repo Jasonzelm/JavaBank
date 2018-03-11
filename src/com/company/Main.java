ca94da9cf27da3a6d791c0f875512a342647a17f
Jason Zelmanovich <jasonzelm7@gmail.com>
2018-03-11 09:54:29 -0400

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Menu bankMenu = new Menu();
+        Menu menu = new Menu();
 
     }
 }
--- src/com/company/Menu.java
+++ src/com/company/Menu.java
@@ -3,21 +3,42 @@ package com.company;
 import java.util.Scanner;
 
 public class Menu {
-
-    Scanner menuScan = new Scanner(System.in);
-    int menuChoice;
-
     public Menu(){
-        do {
-            System.out.println("Choose the corresponding number: \n1- Enter account info\n2- Make a new account\n3- quit the program");
-            menuChoice = menuScan.nextInt();
-            if(menuChoice == 1){
-                Accounts account = new Accounts();
-                break;
-            } //else if(menuChoice == 2){
-                //this will be the option to create an account with an assigned account number but customizable pin number
-            //}
-        }while(menuChoice != 3);
+        //boolean variable automatically set to false that will keep the while loop running until set to true
+        boolean quit = false;
+        //variable to store entered choice through a scanner
+        int scanned ;
+        //Scanner variable in
+        Scanner in = new Scanner(System.in);
+        printMenu();
 
+        while(!quit) {
+            System.out.println("Enter your choice: ");
+            scanned = in.nextInt();
+
+            switch (scanned) {
+                case 0:
+                    printMenu();
+                    break;
+                case 1:
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+                    System.out.println("GoodBye");
+                    quit = true;
+                    break;
+            }
+        }
+    }
+
+    public static void printMenu(){
+        System.out.print("Menu:\n");
+        System.out.print("\t0 - To print the menu\n");
+        System.out.print("\t1 - To Access an existing account\n");
+        System.out.print("\t2 - To create a new account\n");
+        System.out.print("\t3 - To quit the program\n");
     }
 }
